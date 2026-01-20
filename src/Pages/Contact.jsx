7f31d8c99aba7cce068bda0d3d87afc23d37cbd5
Abhishek <abhishek@example.com>
2026-01-21 02:38:23 +0530

    contact page apdate

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -1,18 +1,29 @@
 import React from "react";
 import "./Contact.css";
-import  myphoto from "../assets/myphoto.jpeg";
+import myphoto from "../assets/myphoto.jpeg";
 
 const Contact = () => {
   return (
     <div className="contactPage">
-      <div className="contactCard">
+      <div className="contactCard devStyle">
 
-        <div className="contactImageBox">
+        {/* LEFT - PHOTO */}
+        <div className="contactLeft">
           <img src={myphoto} alt="Vidhya Sagar" className="profileImage" />
+        </div>
 
+        {/* RIGHT - DETAILS */}
+        <div className="contactRight">
           <h2 className="contactName">Vidhya Sagar Kushwaha</h2>
-          <p className="contactEmail">abhishekhkushwah.118@gmail.com</p>
-          <p className="contactPhone">📞 6367697913</p>
+          <p className="contactEnroll">Enrollment No: <span>MGCU2024CSIT3029</span></p>
+
+          <p className="contactInfo">📧 abhishekhkushwah.118@gmail.com</p>
+          <p className="contactInfo">📞 6367697913</p>
+
+          <p className="devQuote">
+            “Turning coffee ☕ into code, bugs into features,  
+            and ideas into scalable web experiences.”
+          </p>
         </div>
 
       </div>
